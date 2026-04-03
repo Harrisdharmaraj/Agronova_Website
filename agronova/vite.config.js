2498dbb065666de556270a9496ef46180e59b4ba
Harrisdharmaraj <129161261+Harrisdharmaraj@users.noreply.github.com>
2026-04-03 15:43:27 +0530

    Update vite.config.js

--- agronova/vite.config.js
+++ agronova/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "180.235.121.245",
+    port: 8032
+  }
 })
